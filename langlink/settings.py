e73846aa13574c1098c83c13f8525198d0194035
hasesho05 <midorinotanuki56@gmail.com>
2023-03-04 19:35:11 +0900

    設定変更

--- langlink/settings.py
+++ langlink/settings.py
@@ -82,18 +82,18 @@ WSGI_APPLICATION = "langlink.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
     # "default": {
-    #     "ENGINE": "django.db.backends.postgresql_psycopg2",
-    #     "NAME": os.environ["DB_NAME"],
-    #     "USER": os.environ["DB_USER"],
-    #     "PASSWORD": os.environ["DB_PASS"],
-    #     "HOST": os.environ["DB_HOST"],
-    #     "PORT": os.environ["DB_PORT"],
-    # }
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # },
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASS"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": os.environ["DB_PORT"],
+    }
 }
 
 
