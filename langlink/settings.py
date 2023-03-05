97d0332d16cf14da3058dadb0781ab48c9e179d6
hasesho05 <midorinotanuki56@gmail.com>
2023-03-05 16:29:29 +0900

    whitenoise削除

--- langlink/settings.py
+++ langlink/settings.py
@@ -41,12 +41,10 @@ INSTALLED_APPS = [
     "rest_framework",
     "drf_spectacular",
     "corsheaders",
-    "ckeditor",
     "app",
 ]
 
 MIDDLEWARE = [
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -82,18 +80,18 @@ WSGI_APPLICATION = "langlink.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # },
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ["DB_NAME"],
-        "USER": os.environ["DB_USER"],
-        "PASSWORD": os.environ["DB_PASS"],
-        "HOST": os.environ["DB_HOST"],
-        "PORT": os.environ["DB_PORT"],
-    }
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql_psycopg2",
+    #     "NAME": os.environ["DB_NAME"],
+    #     "USER": os.environ["DB_USER"],
+    #     "PASSWORD": os.environ["DB_PASS"],
+    #     "HOST": os.environ["DB_HOST"],
+    #     "PORT": os.environ["DB_PORT"],
+    # }
 }
 
 
